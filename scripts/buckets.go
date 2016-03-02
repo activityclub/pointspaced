ad1966c3e643222d3ac7003b3962898a501d126b
Andrew Arrow <andrew@0x7a69.net>
2016-03-02 15:43:26 -0800

    dev

--- scripts/buckets.go
+++ scripts/buckets.go
@@ -8,15 +8,28 @@ import (
 
 func main() {
 	fmt.Println("hi")
+
+	//timestamp := time.Now().Unix()
+
+	addToBucket("327", "2016-03-02", 10)
+	addToBucket("327", "2016-03-02 9", 10)
+
+	addToBucket("327", "2016-03-02", 15)
+	addToBucket("327", "2016-03-02 10", 15)
+
+	addToBucket("327", "2016-03-02", 20)
+	addToBucket("327", "2016-03-02 11", 20)
+}
+
+func addToBucket(user, bucket string, val int) {
 	pool := NewRedisPool(":6379")
 	r := pool.Get()
-
 	key := "psd:"
-	key = key + "327" + ":" + "0" + ":" + "1" + ":" + "123"
-	value := 55
-	timestamp := time.Now().Unix()
-	r.Do("ZINCRBY", key, value, timestamp)
-	fmt.Println("bye ")
+	strAType := "0"
+	flavor := "steps"
+	key = key + user + ":" + strAType + ":" + flavor + ":" + bucket
+	w, ww := r.Do("INCRBY", key, val)
+	fmt.Println("bye ", w, ww)
 	r.Close()
 }
 
