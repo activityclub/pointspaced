6b941e7715761cfb419f16a469f236cf9c758a2b
Andrew Arrow <andrew@0x7a69.net>
2016-03-07 16:41:28 -0800

    logic

--- persistence/redis_simple_sums.go
+++ persistence/redis_simple_sums.go
@@ -3,6 +3,7 @@ package persistence
 import (
 	"fmt"
 	"pointspaced/psdcontext"
+	"strconv"
 	"time"
 )
 
@@ -19,9 +20,10 @@ func ReadBuckets(uids []int64, metric string, aTypes []int64, start_ts int64, en
 	qr.UserToSum["327"] = 2342342
 	qr.UserToSum["1"] = 12342342
 
-	buckets := bucketsForRange(start_ts, end_ts)
-
-	fmt.Println("hi ", buckets)
+	for uid := range uids {
+		buckets := bucketsForRange(int64(uid), start_ts, end_ts)
+		fmt.Println("hi ", buckets)
+	}
 	return qr
 }
 
@@ -35,10 +37,12 @@ func bucket_with_hour(t time.Time, hour int) string {
 	return fmt.Sprintf("%s%02d", format, hour)
 }
 
-func bucketsForRange(start_ts int64, end_ts int64) []string {
+func bucketsForRange(uid, start_ts, end_ts int64) []string {
 	list := make([]string, 0)
 
-	for _, b := range day_buckets_before_full_days() {
+	from := time.Unix(start_ts, 0)
+
+	for _, b := range day_buckets_before_full_days(uid, from) {
 		list = append(list, b)
 	}
 	for _, b := range full_day_buckets() {
@@ -51,9 +55,20 @@ func bucketsForRange(start_ts int64, end_ts int64) []string {
 	return list
 }
 
-func day_buckets_before_full_days() []string {
-	temp := []string{"test", "Test2"}
-	return temp
+func day_buckets_before_full_days(uid int64, from time.Time) []string {
+	list := make([]string, 0)
+	hour := from.Hour()
+	for {
+		if hour > 23 {
+			break
+		}
+		bucket := bucket_with_hour(from, hour)
+		list = append(list, makeKey(uid, bucket))
+		hour += 1
+		from = from.Add(time.Hour)
+	}
+
+	return list
 }
 
 func full_day_buckets() []string {
@@ -64,4 +79,12 @@ func full_day_buckets() []string {
 func day_buckets_after_full_days() []string {
 	temp := []string{"test2", "Test2"}
 	return temp
+}
+
+func makeKey(uid int64, bucket string) string {
+	key := "psd:"
+	strAType := "0"
+	flavor := "steps"
+	key = key + strconv.FormatInt(uid, 10) + ":" + strAType + ":" + flavor + ":" + bucket
+	return key
 }
