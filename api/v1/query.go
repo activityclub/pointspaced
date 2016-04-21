14c5b1dc917bcb9c1f806647fe4969420aa72b8d
Andrew Arrow <andrew@0x7a69.net>
2016-04-20 17:32:15 -0700

    query_fix

--- api/v1/query.go
+++ api/v1/query.go
@@ -3,52 +3,35 @@ package v1
 import "github.com/gin-gonic/gin"
 import "pointspaced/persistence"
 import "strconv"
-import "fmt"
-
-//import "strings"
+import "strings"
 
 func Query(c *gin.Context) {
 
 	// r.GET("/v1/query/:dids/:tzs/:uids/:gids/:aids/:sids/:thing/:start_ts/:end_ts", v1.Query)
 
-	/*
-		dids := c.Param("dids")
-		tzs := c.Param("tzs")
-		uids := c.Param("uids")
-		gids := c.Param("gids")
-		aids := c.Param("aids")
-		sids := c.Param("sids") */
+	group := c.Query("group")
+	if group == "" {
+		c.JSON(406, "group by what?")
+		return
+	}
+
+	mm := persistence.NewMetricManagerHZ()
+	opts := make(map[string][]string)
+	opts["dids"] = strings.Split(c.Param("dids"), ",")
+	opts["tzs"] = strings.Split(c.Param("tzs"), ",")
+	opts["uids"] = strings.Split(c.Param("uids"), ",")
+	opts["gids"] = strings.Split(c.Param("gids"), ",")
+	opts["aids"] = strings.Split(c.Param("aids"), ",")
+	opts["sids"] = strings.Split(c.Param("sids"), ",")
+
 	thing := c.Param("thing")
 	start_ts := c.Param("start_ts")
 	end_ts := c.Param("end_ts")
 
-	group := c.Query("group")
-	if group == "" {
-		group = "uids"
-	}
-
-	/*
-		var uids = []int64{}
-
-		for _, i := range strings.Split(uidStr, ",") {
-			j, _ := strconv.ParseInt(i, 10, 64)
-			uids = append(uids, j)
-		}
-
-		var atypes = []int64{}
-		for _, i := range strings.Split(activity_types, ",") {
-			j, _ := strconv.ParseInt(i, 10, 64)
-			atypes = append(atypes, j)
-		}
-	*/
-
 	start_ts_int, _ := strconv.ParseInt(start_ts, 10, 64)
 	end_ts_int, _ := strconv.ParseInt(end_ts, 10, 64)
 
-	mm := persistence.NewMetricManagerHZ()
-	temp := []int64{0}
-	qr := mm.ReadBuckets(temp, thing, temp, start_ts_int, end_ts_int)
-	fmt.Println(qr)
+	qr := mm.QueryBuckets(thing, group, opts, start_ts_int, end_ts_int)
 
 	c.JSON(200, qr)
 }
