3f3809b9e63d6681762be9152001468c0ff7044e
Andrew Arrow <andrew@0x7a69.net>
2016-06-07 23:09:28 +0000

    adding new post for readcount_with_ids

--- api/v1/query.go
+++ api/v1/query.go
@@ -4,6 +4,8 @@ import "github.com/gin-gonic/gin"
 import "pointspaced/persistence"
 import "strconv"
 import "fmt"
+import "strings"
+import "encoding/json"
 
 func Query(c *gin.Context) {
 
@@ -35,4 +37,24 @@ func ReadCount(c *gin.Context) {
 	end_ts_int, _ := strconv.ParseInt(end_ts, 10, 64)
 	sum := cm.ReadCount(thing, start_ts_int, end_ts_int)
 	c.String(200, fmt.Sprintf("%d", sum))
+}
+
+func ReadCountWithIds(c *gin.Context) {
+	cm := persistence.NewCountManagerHZ()
+	start_ts := c.Param("start_ts")
+	end_ts := c.Param("end_ts")
+	prefix := c.Param("prefix")
+	ids := strings.Split(c.PostForm("ids"), ",")
+
+	start_ts_int, _ := strconv.ParseInt(start_ts, 10, 64)
+	end_ts_int, _ := strconv.ParseInt(end_ts, 10, 64)
+
+	data := make(map[string]int64)
+	for _, id := range ids {
+		data[id] = cm.ReadCount(prefix+"_"+id, start_ts_int, end_ts_int)
+	}
+
+	bytes, _ := json.Marshal(data)
+
+	c.String(200, string(bytes))
 }
--- router/router.go
+++ router/router.go
@@ -7,5 +7,6 @@ func ConfigureRoutes(r *gin.Engine) {
 	r.GET("/health/ping", v1.Ping)
 	r.GET("/v1/query/:uid/:thing/:atid/:start_ts/:end_ts", v1.Query)
 	r.GET("/v1/readcount/:thing/:start_ts/:end_ts", v1.ReadCount)
+	r.POST("/v1/readcount_with_ids/:prefix/:start_ts/:end_ts", v1.ReadCountWithIds)
 	r.POST("/v1/write", v1.Write)
 }
