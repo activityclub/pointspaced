b041fba27ddd816fd94e98a7a3cc6e7e32c69efe
Andrew Arrow <andrew@0x7a69.net>
2016-06-12 20:56:10 +0000

    route

--- router/router.go
+++ router/router.go
@@ -6,6 +6,7 @@ import "pointspaced/api/v1"
 func ConfigureRoutes(r *gin.Engine) {
 	r.GET("/health/ping", v1.Ping)
 	r.GET("/v1/query/:uid/:thing/:atid/:start_ts/:end_ts", v1.Query)
+	r.POST("/v1/query_ids/:thing/:atid/:start_ts/:end_ts", v1.QueryWithIds)
 	r.GET("/v1/readcount/:thing/:start_ts/:end_ts", v1.ReadCount)
 	r.POST("/v1/readcount_with_ids/:prefix/:start_ts/:end_ts", v1.ReadCountWithIds)
 	r.POST("/v1/write", v1.Write)
